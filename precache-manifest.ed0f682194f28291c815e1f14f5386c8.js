22df24e069a0564096c8993cd3468174cbf8801e
mandolyte <cecil.new@gmail.com>
2020-02-11 10:16:17 -0500

    Updates

---
+++ precache-manifest.ed0f682194f28291c815e1f14f5386c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690b6bcf92d3dfe03cbfc2f6cea61a13",
+    "url": "/book-package-app/index.html"
+  },
+  {
+    "revision": "c9744d9f84b690befaa0",
+    "url": "/book-package-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f69c581ede4516b25b14",
+    "url": "/book-package-app/static/js/2.e0ffecf9.chunk.js"
+  },
+  {
+    "revision": "e3d79feaf58f8725d11c611a34d670f8",
+    "url": "/book-package-app/static/js/2.e0ffecf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9744d9f84b690befaa0",
+    "url": "/book-package-app/static/js/main.243efd85.chunk.js"
+  },
+  {
+    "revision": "df202d93e3d561e4d676",
+    "url": "/book-package-app/static/js/runtime-main.d8cbb9cd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/book-package-app/precache-manifest.f308e2b6f6d2367ba63c5f95cfa6f0de.js"
+  "/book-package-app/precache-manifest.ed0f682194f28291c815e1f14f5386c8.js"
 );
 
 self.addEventListener('message', (event) => {
